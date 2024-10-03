2c04b839faab1278e3cd11d9591231571d6abc01
Mehran <mehranbhat2001@gmail.com>
2024-10-03 15:23:18 +0530

    operators c++

--- first.cpp
+++ first.cpp
@@ -1,14 +1,29 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout << "please enter your age ! "<< endl;
+//     cin >> age ;
+//     cout << " your age is :" << age << endl;
+
+//     int distance ;
+//     cout << "what is the distanec from srinagar to jammu\n";
+//     cin >> distance;
+//     cout <<"the total distanec is :" << distance << endl;
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 int main(){
-    int age;
-    cout << "please enter your age ! "<< endl;
-    cin >> age ;
-    cout << " your age is :" << age << endl;
+   int a = 9, b = 5;
+   
+   cout << a + b << endl;
+   cout << a - b << endl;
+   cout << a * b << endl;
+   cout << a / b << endl;
 
-    int distance ;
-    cout << "what is the distanec from srinagar to jammu\n";
-    cin >> distance;
-    cout <<"the total distanec is :" << distance << endl;
+   cout << (5 != 5) << endl;
+cout << !(10 > 12) << endl;
+cout << ((8 > 7) && (10 < 20)) << endl;
     return 0;
 }
