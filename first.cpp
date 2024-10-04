111b495bf76f552ab17a0dd765c1b259e0b6db9f
Mehran <mehranbhat2001@gmail.com>
2024-10-04 14:54:43 +0530

    if else c++

--- first.cpp
+++ first.cpp
@@ -12,11 +12,30 @@
 //     cout <<"the total distanec is :" << distance << endl;
 //     return 0;
 // }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cout << "enetr a :" ;
+//     cin >> a;
+//     cout << "enetr b :" ;
+//     cin >> b;
+//     int divide = a % b;
+//     cout<< divide << endl;
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 int main(){
-int a = 4;
-cout << (++a) << endl;
-cout << a << endl;
+    int age ;
+   cout << "Enter your age\n";
+   cin >> age;
+    if(age >= 18){
+        cout << "you can Vote\n";
+    }else{
+        cout << "you are not eligible to Vote\n";
+    }
     return 0;
 }
