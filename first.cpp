48e34f8170ae59359d0e1f91cee29e601584a37e
Mehran <mehranbhat2001@gmail.com>
2024-10-05 15:16:40 +0530

    Prime numbers DSA

--- first.cpp
+++ first.cpp
@@ -137,21 +137,21 @@
 //   cout<< sum <<endl;
 //     return 0;
 // }
-#include <iostream>
-using namespace std;
-int main(){
-  int n = 10;
-int i = 0;
-int sum = 0;
-while(i <= n){
-  if(i % 2 == 0){
-    sum = sum + i;
-  }
-  i++;
-}
-cout << sum << endl;
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n = 10;
+// int i = 0;
+// int sum = 0;
+// while(i <= n){
+//   if(i % 2 == 0){
+//     sum = sum + i;
+//   }
+//   i++;
+// }
+// cout << sum << endl;
+//     return 0;
+// }
 
 
   // int n = 10;
@@ -171,5 +171,20 @@ cout << sum << endl;
 
 
 
-
+#include <iostream>
+using namespace std;
+int main(){
+  int n = 9;
+ 
+ for(int i = 2; i <= (n-1); i++){
+  if(n % i == 0){
+    cout << n << " is not a prime number.\n";
+    return 0;
+  }else{
+    cout << n << " is a prime number.\n";
+    break;
+  }
+ }
+  return 0;
+}
 
