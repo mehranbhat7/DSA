e3001536735d8915b51b8f5d8f338636cdc58972
Mehran <mehranbhat2001@gmail.com>
2024-10-07 12:04:45 +0530

    Diamond patterns DSA

--- first.cpp
+++ first.cpp
@@ -216,18 +216,32 @@
 //   }
 //   return 0;
 // }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n = 3;
+//   char ch= 'A';
+ 
+//  for(int i =0;i<n;i++){
+//   for(int j =0;j<n;j++){
+//     cout << ch<< "   ";
+//    ch++;
+//  }
+//  cout << endl;
+//  }
+//   return 0;
+// }
 #include <iostream>
 using namespace std;
 int main(){
-  int n = 3;
-  char ch= 'A';
- 
- for(int i =0;i<n;i++){
-  for(int j =0;j<n;j++){
-    cout << ch<< "   ";
-   ch++;
- }
- cout << endl;
- }
+  int n = 10;
+  int num = 1;
+  for(int i=0;i<n;i++){
+    for(int j =0;j<i+1;j++){
+      cout << num << " ";
+      num++;
+    }
+    cout << endl;
+  }
   return 0;
 }
