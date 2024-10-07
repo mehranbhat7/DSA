31adf0c73513e5993717cfef12ee5bae745b7382
Mehran <mehranbhat2001@gmail.com>
2024-10-07 12:44:57 +0530

    DIamond Patterns DSA

--- first.cpp
+++ first.cpp
@@ -234,14 +234,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int n = 10;
-  int num = 1;
-  for(int i=0;i<n;i++){
-    for(int j =0;j<i+1;j++){
-      cout << num << " ";
-      num++;
-    }
-    cout << endl;
+int n = 5;
+char ch = 'A';
+for(int i=0;i<n;i++){
+  for(int j=0;j<i+1;j++){
+    cout << ch ; 
+   
   }
+  cout << endl;
+  ch++;
+}
   return 0;
 }
