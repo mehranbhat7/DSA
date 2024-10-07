8d52812a3e61256a97a8f31c4f5fae9a40ce18c3
Mehran <mehranbhat2001@gmail.com>
2024-10-07 11:42:22 +0530

    Different Approach DSA

--- first.cpp
+++ first.cpp
@@ -202,17 +202,32 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n = 15;
+//   for(int i =0;i<n;i++){
+//     char ch = 'A';
+//     for(int j=0;j<n;j++){
+//       cout << ch << " ";
+//       ch++;
+//     }
+//     cout << endl;
+//   }
+//   return 0;
+// }
 #include <iostream>
 using namespace std;
 int main(){
-  int n = 15;
-  for(int i =0;i<n;i++){
-    char ch = 'A';
-    for(int j=0;j<n;j++){
-      cout << ch << " ";
-      ch++;
-    }
-    cout << endl;
-  }
+  int n = 3;
+  char ch= 'A';
+ 
+ for(int i =0;i<n;i++){
+  for(int j =0;j<n;j++){
+    cout << ch<< "   ";
+   ch++;
+ }
+ cout << endl;
+ }
   return 0;
 }
