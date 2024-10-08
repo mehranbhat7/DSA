409bae53d1444ae1c73e4834001b8b3b4a7244d1
Mehran <mehranbhat2001@gmail.com>
2024-10-08 12:10:20 +0530

    Pyramid things

--- first.cpp
+++ first.cpp
@@ -288,16 +288,35 @@
 // }
 //   return 0;
 // }
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n = 5;
+// for(int i = 0; i<n;i++){
+//   for(int j = 0; j < i; j++){
+//     cout << " ";
+//   }
+//   for(int j =0; j< n-i; j++){
+//     cout << i+ 1 << " ";
+//   }
+//   cout << endl;
+// }
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
 int n = 5;
-for(int i = 0; i<n;i++){
-  for(int j = 0; j < i; j++){
+for(int i =0;i<n;i++){
+  for( int j=0;j<=n-i-1;j++){
     cout << " ";
   }
-  for(int j =0; j< n-i; j++){
-    cout << i+ 1 << " ";
+  for(int j=1;j <= i+1;j++){
+    cout << j;
+  }
+  for(int j= i; j >0;j--){
+    cout << j;
   }
   cout << endl;
 }
