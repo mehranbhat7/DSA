7f591564bcf142c7f799878ad44d4963a223afc9
Mehran <mehranbhat2001@gmail.com>
2024-10-09 12:15:25 +0530

    DSa practice

--- first.cpp
+++ first.cpp
@@ -365,17 +365,32 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int  n = 4;
+//   int num = 1;
+//   for(int i =0;i<n;i++){
+//     for(int j=0;j< i+1;j++){
+//       cout << j+1;
+//     }
+//     cout << endl;
+      
+//   }
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
   int  n = 4;
-  int num = 1;
+  int num =1;
   for(int i =0;i<n;i++){
-    for(int j=0;j< i+1;j++){
-      cout << j+1;
+    for(int j=1;j<=i+1;j++){
+      cout << num++;
     }
     cout << endl;
-      
   }
+
   return 0;
 }
