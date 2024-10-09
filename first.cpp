946b5392bdc9ba1324dfecae9f45d62384757414
Mehran <mehranbhat2001@gmail.com>
2024-10-09 11:52:18 +0530

    DSA patterns

--- first.cpp
+++ first.cpp
@@ -350,6 +350,21 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int  n = 4;
+//   int num = 1;
+//   for(int i =0;i<n;i++){
+//     for(int j=0;j< i+1;j++){
+//       cout << num;
+//     }
+//     cout << endl;
+//       num++;
+//   }
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -357,10 +372,10 @@ int main(){
   int num = 1;
   for(int i =0;i<n;i++){
     for(int j=0;j< i+1;j++){
-      cout << num;
+      cout << j+1;
     }
     cout << endl;
-      num++;
+      
   }
   return 0;
 }
