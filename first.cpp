d37ae040660ee0043a99fcfe29dc49a883a8858e
Mehran <mehranbhat2001@gmail.com>
2024-10-10 11:13:02 +0530

    Pyramid

--- first.cpp
+++ first.cpp
@@ -395,19 +395,41 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n =4;
+//   char ch ='A';
+//   for(int i =0; i< n;i++){
+//     for(int j =0; j <i ;j++){
+//       cout << " ";
+//     }
+//     for(int j=0;j<n-i;j++){
+//       cout << ch;
+//     }
+    
+//     cout << endl;
+//     ch++;
+//   }
+//   return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 int main(){
   int n =4;
-  for(int i =0; i< n;i++){
-    for(int j =0; j <i ;j++){
-      cout << " ";
-    }
-    for(int j=0;j<n-i;j++){
-      cout << i+1;
-    }
-    
-    cout << endl;
+for(int i =0;i <n;i++){
+  for(int j =0;j<n-i-1;j++){
+    cout << " ";
   }
+  for(int j=1;j<=i+1;j++){
+    cout << j;
+  }
+ for(int j=i;j>0;j--){
+  cout << j ;
+ }
+ cout << endl;
+}
   return 0;
 }
