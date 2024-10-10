ba104c697d716c7cb908f7d05452f6d708a7b741
Mehran <mehranbhat2001@gmail.com>
2024-10-10 10:40:39 +0530

    Patterns DSA

--- first.cpp
+++ first.cpp
@@ -401,8 +401,8 @@ int main(){
   int n =6;
   char ch = 'A';
   for(int i=0;i<n;i++){
-    for(int j=0;j<i+1;j++){
-      cout << ch;
+    for(int j=1;j<=i+1;j++){
+      cout << j;
     }
     cout << endl;
     ch++;
