cde519b5a982435c20a1e125242e09800856b0c9
Mehran <mehranbhat2001@gmail.com>
2024-10-10 11:47:23 +0530

    Holloow diamond

--- first.cpp
+++ first.cpp
@@ -417,19 +417,53 @@
 
 #include <iostream>
 using namespace std;
-int main(){
-  int n =4;
-for(int i =0;i <n;i++){
-  for(int j =0;j<n-i-1;j++){
-    cout << " ";
-  }
-  for(int j=1;j<=i+1;j++){
-    cout << j;
-  }
- for(int j=i;j>0;j--){
-  cout << j ;
- }
- cout << endl;
-}
-  return 0;
+
+int main() {
+    int n = 4;
+
+    // Upper part of the pattern
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Print the first star
+        cout << "*";
+
+        // Print the hollow space and second star for rows after the first
+        if (i != 0) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        // Move to the next line
+        cout << endl;
+    }
+
+    // Lower part of the pattern
+    for (int i = 0; i < n - 1; i++) {
+        // Print leading spaces
+        for (int j = 0; j < i + 1; j++) {
+            cout << " ";
+        }
+
+        // Print the first star
+        cout << "*";
+
+        // Print the hollow space and second star for rows before the last
+        if (i != n - 2) {
+            for (int j = 0; j < 2 * (n - i - 2) - 1; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        // Move to the next line
+        cout << endl;
+    }
+
+    return 0;
 }
