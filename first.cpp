3469b61e68b44dc802c8479ab1a303e2c320faa5
Mehran <mehranbhat2001@gmail.com>
2024-10-11 11:26:30 +0530

    Functions DSA

--- first.cpp
+++ first.cpp
@@ -395,24 +395,39 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int n =5;
+//   for(int i=0;i<n;i++){
+//     for(int j=0;j<n-i-1;j++){
+//         cout << " ";
+//     }
+//     for(int j =1;j<=i+1;j++){
+//         cout << j ;
+//     }
+    
+//         for(int j=i;j>0;j--){
+//             cout << j;
+//         }
+    
+//     cout << endl;
+//   }
+ 
+// }
 #include <iostream>
 using namespace std;
+int name(){
+    cout << "Mehran\n";
+    return 0;
+}
 int main(){
-  int n =5;
-  for(int i=0;i<n;i++){
-    for(int j=0;j<n-i-1;j++){
-        cout << " ";
-    }
-    for(int j =1;j<=i+1;j++){
-        cout << j ;
-    }
-    
-        for(int j=i;j>0;j--){
-            cout << j;
-        }
-    
-    cout << endl;
-  }
+  name();
+  name();
+  name();
+  name();
+  name();
+  return 0;
  
 }
 
