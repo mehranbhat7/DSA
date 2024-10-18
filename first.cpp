3de82359cb73aba88f0490b41043880c763c971a
Mehran <mehranbhat2001@gmail.com>
2024-10-18 17:34:01 +0530

    Array Sum and Product

--- first.cpp
+++ first.cpp
@@ -659,24 +659,42 @@
 //   return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// void reverseArr(int arr[], int size){
+// int start = 0;
+// int end = size -1;
+// while(start < end){
+//   swap(arr[start],arr[end]);
+//   start++;
+//   end--;
+// }
+// }
+// int main(){
+//   int arr[] ={3,5,7,2,8,1};
+//   int size = 6;
+//   reverseArr(arr,size);
+//   for(int i =0;i<size;i++){
+//     cout << arr[i] << " ";
+//   }
+//   cout << endl;
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
-void reverseArr(int arr[], int size){
-int start = 0;
-int end = size -1;
-while(start < end){
-  swap(arr[start],arr[end]);
-  start++;
-  end--;
+int reverseArr(int arr[], int size){
+int prod = 1;
+for(int i = 0; i< size ; i++){
+  prod = prod * arr[i];
 }
+return prod;
+
 }
 int main(){
   int arr[] ={3,5,7,2,8,1};
   int size = 6;
-  reverseArr(arr,size);
-  for(int i =0;i<size;i++){
-    cout << arr[i] << " ";
-  }
-  cout << endl;
+ int total = reverseArr(arr, size) ;
+ cout << total << endl;
   return 0;
 }
