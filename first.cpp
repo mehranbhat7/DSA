e97088bf1e44d64edaeb69c1be55f64ad22c6a62
Mehran <mehranbhat2001@gmail.com>
2024-10-21 17:53:46 +0530

    DSA loops

--- first.cpp
+++ first.cpp
@@ -740,11 +740,14 @@ using namespace std;
 // }
 
 int main(){
-char test = 'b';
-if(test >= 'a' && test <= 'z' ){
-  cout << "lowercase" << endl;
-}else{
-  cout << "uppercase" << endl;
+int n = 7;
+int sum = 0;
+for(int i=0; i<=n; i++){
+  if(i % 2 != 0){
+    sum = sum + i;
+  }
 }
+cout << "Sum: " << sum << endl;
+
 return 0;
 }
