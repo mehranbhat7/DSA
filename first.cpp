a945f496c561c6fd402e5f5788503e3d9959fa03
Mehran <mehranbhat2001@gmail.com>
2024-10-25 16:00:40 +0530

    Reverse an array

--- first.cpp
+++ first.cpp
@@ -759,15 +759,16 @@ using namespace std;
 int main(){
   int arr[] = {3,6,9,2,7,5};
   int size = 6;
- 
-  int small = INT_MIN;
-  for(int i =0; i < size; i++){
-   if(arr[i] > small){
-    small = arr[i];
-    ind = i;
-   }
-  }
-  cout << small << endl;
-  cout << ind << endl;
+ int start =  0 ;
+ int end = size -1;
+ while(start < end){
+  swap(arr[start], arr[end]);
+  start++;
+  end--;
+ }
+ for(int i =0; i< size; i++){
+  cout << arr[i] << endl;
+
+ }
   return 0;
 }
