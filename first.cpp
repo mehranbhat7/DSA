7ea56954b480afa646b5bc4129447ed8b2d896ab
Mehran <mehranbhat2001@gmail.com>
2024-10-26 15:43:25 +0530

    Vectors

--- first.cpp
+++ first.cpp
@@ -754,21 +754,29 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int arr[] = {3,6,9,2,7,5};
+//   int size = 6;
+// int sum = 0;
+// for(int i = 0; i < size; i++){
+//   sum = sum + arr[i];
+// }
+// cout << "Sum: " << sum << endl;
+  
+//   return 0;
+// }
+
+
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
-  int arr[] = {3,6,9,2,7,5};
-  int size = 6;
- int start =  0 ;
- int end = size -1;
- while(start < end){
-  swap(arr[start], arr[end]);
-  start++;
-  end--;
+ vector<int> arr = {1,2,3,4,5,6,7,8};
+ for(int val : arr){
+   cout << val << endl;
  }
- for(int i =0; i< size; i++){
-  cout << arr[i] << endl;
-
- }
+  
   return 0;
 }
