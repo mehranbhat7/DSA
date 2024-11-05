d9ed373b2e7a152c3b6e647c85000de8d7b53855
Mehran <mehranbhat2001@gmail.com>
2024-11-05 17:35:26 +0530

    Maximum of SUbarray

--- first.cpp
+++ first.cpp
@@ -789,20 +789,43 @@
 // }
 
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main(){
+//  int arr[] = {1,2,3,4,5};
+//  int n = 5;
+//   for(int st = 0; st< n; st++){
+//     for(int end = st; end < n; end++){
+//       for(int i = st; i <=end; i++)
+//       cout << arr[i] << " ";
+//     }
+//     cout << " ";
+//   }
+//   cout << endl;
+
+//   return 0;
+// }
+
 #include <iostream>
 #include <vector>
 using namespace std;
 int main(){
- int arr[] = {1,2,3,4,5};
- int n = 5;
+ int arr[] = {3,-4,5,4,-1,7,-8};
+ int n = 7;
+ int maxSum = INT_MIN;
   for(int st = 0; st< n; st++){
+    int sum = 0;
     for(int end = st; end < n; end++){
-      for(int i = st; i <=end; i++)
-      cout << arr[i] << " ";
+      sum = sum + arr[end];
+      maxSum = max(sum , maxSum);
     }
-    cout << " ";
+  
   }
-  cout << endl;
+  cout << "Maximum Sum: " << maxSum << endl;
+
+  
+ 
 
   return 0;
 }
