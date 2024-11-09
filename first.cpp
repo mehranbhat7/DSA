aab95016e5549d73e6ab2cca7adffd10d9d72808
Mehran <mehranbhat2001@gmail.com>
2024-11-09 12:52:15 +0530

    Time Complexities

--- first.cpp
+++ first.cpp
@@ -855,15 +855,25 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int arr[] = {2,3,1,5,7};
+//   int n = 5;
+//   int sum = 0;
+//   for(int i = 0; i< n;i++){
+//     sum = sum + arr[i];
+// }
+//   cout << sum << endl;
+//   return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
   int arr[] = {2,3,1,5,7};
   int n = 5;
-  int sum = 0;
-  for(int i = 0; i< n;i++){
-    sum = sum + arr[i];
-}
-  cout << sum << endl;
+int lastIdx = arr[0];
+cout << lastIdx << endl;
   return 0;
 }
